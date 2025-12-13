2b7e368626943ae8cfdb01c809a660e483a68f40
ShadowGXYT <alhswiss@gmail.com>
2025-12-13 12:32:38 +0100

    fdfpj

--- course_python/Module_1/Lection_3/task1.py
+++ course_python/Module_1/Lection_3/task1.py
@@ -2,23 +2,23 @@ import telebot
 
     # Замени 'TOKEN' на токен твоего бота
     # Этот токен ты получаешь от BotFather, чтобы бот мог работать
-    bot = telebot.TeleBot("8160895447:AAF8mFVOieZrH_gqrMPxVMSDse8O_7maLdM")
+bot = telebot.TeleBot("8160895447:AAF8mFVOieZrH_gqrMPxVMSDse8O_7maLdM")
     
-    @bot.message_handler(commands=['start'])
-    def send_welcome(message):
-        bot.reply_to(message, "Привет! Я твой Telegram бот. Напиши что-нибудь!")
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(message, "Привет! Я твой Telegram бот. Напиши что-нибудь!")
     
-    @bot.message_handler(commands=['hello'])
-    def send_hello(message):
-        bot.reply_to(message, "Привет! Как дела?")
+@bot.message_handler(commands=['hello'])
+def send_hello(message):
+    bot.reply_to(message, "Привет! Как дела?")
     
-    @bot.message_handler(commands=['bye'])
-    def send_bye(message):
-        bot.reply_to(message, "Пока! Удачи!")
+@bot.message_handler(commands=['bye'])
+def send_bye(message):
+    bot.reply_to(message, "Пока! Удачи!")
     
     
-    @bot.message_handler(func=lambda message: True)
-    def echo_all(message):
-        bot.reply_to(message, message.text)
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+    bot.reply_to(message, message.text)
     
-    bot.polling()
+bot.polling()
